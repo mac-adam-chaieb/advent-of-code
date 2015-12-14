b081afab82526f897ae097089b3960791f6ec2f9
Mohamed-Adam Chaieb <moe.chaieb@shopify.com>
2015-12-14 15:10:45 -0500

    Day 5

--- day5.rb
+++ day5.rb
@@ -1,1 +1,7 @@
-t = File.open('data/day5.txt', 'r').readlines
+t = File.open('data/day5.txt', 'r').readlines.map(&:strip)
+a,p = ('a'..'z').to_a.zip('a'..'z').map(&:join), ['ab','cd','pq','xy']
+n = t.select {|w| w.scan(/[aeiou]/).size > 2 && p.all? {|p| !w.include? p} && a.any? {|p| w.include? p}}
+m = t.select {|w| w.chars.each_cons(2).any?{|p|w.scan(p.join).size > 1} && w.chars.each_cons(3).any?{|p| p[0] == p[2]}}
+
+p "There are #{n.size} nice strings in his text file."
+p "After Santa reviews his model, there are #{m.size} nice string in his text file."
